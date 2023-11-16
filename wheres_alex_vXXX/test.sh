006ab0c7d27a109889b6b05b5ea860a3d082a379
dark horse <asharma13524@gmail.com>
2023-11-16 08:49:54 -0500

    wip on accept game test

--- wheres_alex_vXXX/test.sh
+++ wheres_alex_vXXX/test.sh
@@ -115,7 +115,57 @@ echo "
 # ########  ###     ### ###       ### ##########
 "
 
-#leo run accept_game
+# leo run accept_game "{
+#   owner: aleo1asu88azw3uqud282sll23wh3tvmvwjdz5vhvu2jwyrdwtgqn5qgqetuvr6.private,
+#   challenger_commit: 1632338669887332693450432112819554759682427161589172463360656597380489639523field.private,
+#   opponent_answer: 0field.private,
+#   total_pot: 200u64.private,
+#   challenger_address: aleo16hf8hfpwasnn9cf7k2c0dllc56nn7qt547qxgvgwu6pznw4trvqsx68kls.private,
+#   opponent_address: aleo1r4pc6ufjvw050jhzrew3vqm2lvacdxfd4a5ckulau0vjc72qvc8sr0jg2a.private,
+#   game_multisig: aleo1asu88azw3uqud282sll23wh3tvmvwjdz5vhvu2jwyrdwtgqn5qgqetuvr6.private,
+#   game_state: 1field.private,
+#   _nonce: 1334964628097769506346609254612928141201659101623645895663814974083190759961group.public
+# }" 0field "{
+#   owner: aleo1asu88azw3uqud282sll23wh3tvmvwjdz5vhvu2jwyrdwtgqn5qgqetuvr6.private,
+#   amount: 100u64.private,
+#   challenger: aleo16hf8hfpwasnn9cf7k2c0dllc56nn7qt547qxgvgwu6pznw4trvqsx68kls.private,
+#   opponent: aleo1r4pc6ufjvw050jhzrew3vqm2lvacdxfd4a5ckulau0vjc72qvc8sr0jg2a.private,
+#   ix: 3u32.private,
+#   _nonce: 4284598271155980028049768842401005159567204128606052241931518995011812618429group.public
+# }" "{
+#   owner: aleo1asu88azw3uqud282sll23wh3tvmvwjdz5vhvu2jwyrdwtgqn5qgqetuvr6.private,
+#   amount: 100u64.private,
+#   claimer: aleo1r4pc6ufjvw050jhzrew3vqm2lvacdxfd4a5ckulau0vjc72qvc8sr0jg2a.private,
+#   message_1: 4299148155668445410404796665478412871867472362428312345599969289162437917169field.private,
+#   message_2: 251561861986387641133995513088278466362266230978294643355666629668300490271field.private,
+#   message_3: 1152742118437172617676919179607096353215168002720611159713055309066048881777field.private,
+#   message_4: 6965356107999508170054292104522311809055538516545196583411503498743541220125field.private,
+#   message_5: 478560413032field.private,
+#   challenger: aleo16hf8hfpwasnn9cf7k2c0dllc56nn7qt547qxgvgwu6pznw4trvqsx68kls.private,
+#   opponent: aleo1r4pc6ufjvw050jhzrew3vqm2lvacdxfd4a5ckulau0vjc72qvc8sr0jg2a.private,
+#   ix: 6u32.private,
+#   _nonce: 3569726119866764549964441851090282924398824450345405697637548871911049598842group.public
+# }" "{
+#   owner: aleo1asu88azw3uqud282sll23wh3tvmvwjdz5vhvu2jwyrdwtgqn5qgqetuvr6.private,
+#   amount: 100u64.private,
+#   challenger: aleo16hf8hfpwasnn9cf7k2c0dllc56nn7qt547qxgvgwu6pznw4trvqsx68kls.private,
+#   opponent: aleo1r4pc6ufjvw050jhzrew3vqm2lvacdxfd4a5ckulau0vjc72qvc8sr0jg2a.private,
+#   ix: 3u32.private,
+#   _nonce: 1971872521381063346261049000143077388486554460249572615590773197484105792568group.public
+# }" "{
+#   owner: aleo1asu88azw3uqud282sll23wh3tvmvwjdz5vhvu2jwyrdwtgqn5qgqetuvr6.private,
+#   amount: 100u64.private,
+#   claimer: aleo16hf8hfpwasnn9cf7k2c0dllc56nn7qt547qxgvgwu6pznw4trvqsx68kls.private,
+#   message_1: 8062328565641143710315198539395259864274213782537700083868207132716559019626field.private,
+#   message_2: 646976134778083579747150617209623060175268802563807996500102649727939562470field.private,
+#   message_3: 7738966642647861988443742254957166327730088714215632067055062293849087980027field.private,
+#   message_4: 501202936879316583063216806269060512965140130553350448375465909870676136661field.private,
+#   message_5: 478560413032field.private,
+#   challenger: aleo16hf8hfpwasnn9cf7k2c0dllc56nn7qt547qxgvgwu6pznw4trvqsx68kls.private,
+#   opponent: aleo1r4pc6ufjvw050jhzrew3vqm2lvacdxfd4a5ckulau0vjc72qvc8sr0jg2a.private,
+#   ix: 6u32.private,
+#   _nonce: 3366954178518918621707747175319544829507693042206066118513504319768518783949group.public
+# }" 10u32
 
 # Swap in the private key of the challenger -- Alice.
 echo "
