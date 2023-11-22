d94da87e21b53dae01e5489ed50e88fe26ea21d3
mwyatt896 <mwyatt896@gmail.com>
2023-11-22 06:38:36 -0800

    fixed signature check in reveal_answer

--- multiparty_pvp_utils_vXXX/test.sh
+++ multiparty_pvp_utils_vXXX/test.sh
@@ -63,3 +63,36 @@ echo "
 leo run mint_multisig_key 12345field 100u64 aleo1asu88azw3uqud282sll23wh3tvmvwjdz5vhvu2jwyrdwtgqn5qgqetuvr6 aleo16hf8hfpwasnn9cf7k2c0dllc56nn7qt547qxgvgwu6pznw4trvqsx68kls aleo1r4pc6ufjvw050jhzrew3vqm2lvacdxfd4a5ckulau0vjc72qvc8sr0jg2a
 
 # TODO -- get a claim signature and test reveal answer
+
+echo "
+#       :::::::::  :::::::::: :::     ::: ::::::::::     :::     :::         
+#      :+:    :+: :+:        :+:     :+: :+:          :+: :+:   :+:          
+#     +:+    +:+ +:+        +:+     +:+ +:+         +:+   +:+  +:+           
+#    +#++:++#:  +#++:++#   +#+     +:+ +#++:++#   +#++:++#++: +#+            
+#   +#+    +#+ +#+         +#+   +#+  +#+        +#+     +#+ +#+             
+#  #+#    #+# #+#          #+#+#+#   #+#        #+#     #+# #+#              
+# ###    ### ##########     ###     ########## ###     ### ##########        
+#           :::     ::::    :::  ::::::::  :::       ::: :::::::::: :::::::::
+#        :+: :+:   :+:+:   :+: :+:    :+: :+:       :+: :+:        :+:    :+:
+#      +:+   +:+  :+:+:+  +:+ +:+        +:+       +:+ +:+        +:+    +:+ 
+#    +#++:++#++: +#+ +:+ +#+ +#++:++#++ +#+  +:+  +#+ +#++:++#   +#++:++#:   
+#   +#+     +#+ +#+  +#+#+#        +#+ +#+ +#+#+ +#+ +#+        +#+    +#+   
+#  #+#     #+# #+#   #+#+# #+#    #+#  #+#+# #+#+#  #+#        #+#    #+#    
+# ###     ### ###    ####  ########    ###   ###   ########## ###    ###     
+"
+
+leo run reveal_answer "{
+    owner: aleo16hf8hfpwasnn9cf7k2c0dllc56nn7qt547qxgvgwu6pznw4trvqsx68kls.private,
+    challenger_address: aleo16hf8hfpwasnn9cf7k2c0dllc56nn7qt547qxgvgwu6pznw4trvqsx68kls.private,
+    opponent_address: aleo1r4pc6ufjvw050jhzrew3vqm2lvacdxfd4a5ckulau0vjc72qvc8sr0jg2a.private,
+    game_address: aleo1asu88azw3uqud282sll23wh3tvmvwjdz5vhvu2jwyrdwtgqn5qgqetuvr6.private,
+    amount: 100u64.private,
+    nonce: 12345field.private,
+    answer: 98765field.private,
+    message_1: 8062328565641143710315198539395259864274213782537700083868207132716559019626field.private,
+    message_2: 646976134778083579747150617209623060175268802563807996500102649727939562470field.private,
+    message_3: 7738966642647861988443742254957166327730088714215632067055062293849087980027field.private,
+    message_4: 501202936879316583063216806269060512965140130553350448375465909870676136661field.private,
+    message_5: 478560413032field.private,
+    _nonce: 3086461441036924895581482603324967423059802832652857085654924791650328692109group.public
+    }" sign1h04lnsl0t4aau8lzp06rzcm5eqrzr9ew63ljnw43v2nr7nkmsqqcx8cksna2ajwgk80rv0s7prrndw5k56sey3nrl487552lvaukuq8xe0cgu6x809qetnn35ufm3gl6ecyvtpgaavu9y5754j27utrwq8amruqyq2x6dvqs790yqsrctwmjnh3k7thslm0r9c3hpdvjrywpzts24xr
