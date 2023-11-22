74ef52d9830e2a4f9fc8e701bf036b59bf3b44e8
mwyatt896 <mwyatt896@gmail.com>
2023-11-22 06:54:40 -0800

    cleanup utils test script

--- multiparty_pvp_utils_vXXX/test.sh
+++ multiparty_pvp_utils_vXXX/test.sh
@@ -23,19 +23,6 @@ echo "
 
 leo run mint_answer aleo16hf8hfpwasnn9cf7k2c0dllc56nn7qt547qxgvgwu6pznw4trvqsx68kls aleo1r4pc6ufjvw050jhzrew3vqm2lvacdxfd4a5ckulau0vjc72qvc8sr0jg2a aleo1asu88azw3uqud282sll23wh3tvmvwjdz5vhvu2jwyrdwtgqn5qgqetuvr6 100u64 12345field 98765field 8062328565641143710315198539395259864274213782537700083868207132716559019626field 646976134778083579747150617209623060175268802563807996500102649727939562470field 7738966642647861988443742254957166327730088714215632067055062293849087980027field 501202936879316583063216806269060512965140130553350448375465909870676136661field 478560413032field sign1h04lnsl0t4aau8lzp06rzcm5eqrzr9ew63ljnw43v2nr7nkmsqqcx8cksna2ajwgk80rv0s7prrndw5k56sey3nrl487552lvaukuq8xe0cgu6x809qetnn35ufm3gl6ecyvtpgaavu9y5754j27utrwq8amruqyq2x6dvqs790yqsrctwmjnh3k7thslm0r9c3hpdvjrywpzts24xr
 
-# Need to add consume answer below
-# [consume_answer]
-# my_game_answer: Answer = Answer {
-#   owner: aleo1asu88azw3uqud282sll23wh3tvmvwjdz5vhvu2jwyrdwtgqn5qgqetuvr6.private,
-#   challenger_address: aleo1asu88azw3uqud282sll23wh3tvmvwjdz5vhvu2jwyrdwtgqn5qgqetuvr6.private,
-#   opponent_address: aleo16hf8hfpwasnn9cf7k2c0dllc56nn7qt547qxgvgwu6pznw4trvqsx68kls.private,
-#   game_address: aleo1r4pc6ufjvw050jhzrew3vqm2lvacdxfd4a5ckulau0vjc72qvc8sr0jg2a.private,
-#   amount: 10u64.private,
-#   nonce: 1235field.private,
-#   answer: 1235field.private,
-#   _nonce: 4769977707015002896095071429795547085553080541391902355864986662188154177175group.public
-# };
-
 echo "
 #         :::   :::   ::::::::::: ::::    ::: :::::::::::                      
 #       :+:+: :+:+:      :+:     :+:+:   :+:     :+:                           
@@ -61,8 +48,6 @@ echo "
 "
 
 leo run mint_multisig_key 12345field 100u64 aleo1asu88azw3uqud282sll23wh3tvmvwjdz5vhvu2jwyrdwtgqn5qgqetuvr6 aleo16hf8hfpwasnn9cf7k2c0dllc56nn7qt547qxgvgwu6pznw4trvqsx68kls aleo1r4pc6ufjvw050jhzrew3vqm2lvacdxfd4a5ckulau0vjc72qvc8sr0jg2a
-
-# TODO -- get a claim signature and test reveal answer
 
 echo "
 #       :::::::::  :::::::::: :::     ::: ::::::::::     :::     :::         
