f08ebc57e67888d9fcab58a08529cade60fb944d
Luke Newman <5315645+lukenewman@users.noreply.github.com>
2024-11-25 23:14:49 -0500

    more work on testing scripts

--- squash/test.sh
+++ squash/test.sh
@@ -1,16 +1,8 @@
 # to swap out private keys:
 # sed -i '' "s/PRIVATE_KEY=.*/PRIVATE_KEY=$ORACLE_KEY/" .env
 
-PLAYER_KEY=APrivateKey1zkp4sc5nWLq2yLiPS8TDLYQayQXAn6VfCC3f8Ft66P6sSkg
-PLAYER_ADDRESS=aleo1r8vz4dk0ku7fkwernv665e05zt5t4xv6mprrhxq3wwa9dpzjkvpsvxv99h
-ORACLE_KEY=APrivateKey1zkp4sc5nWLq2yLiPS8TDLYQayQXAn6VfCC3f8Ft66P6asdf
+PLAYER_ADDRESS=aleo1x4qm9ycjnm4uht6a7jvvl8v6vu20hhx864zagdtp34lscx6rhs9s8m3834
 DELTA=600u64
-
-echo "
-NETWORK=testnet
-PRIVATE_KEY=$PLAYER_KEY
-ENDPOINT=https://node.puzzle.online
-" > .env
 
 extract_outputs() {
   local output="$1"
@@ -36,29 +28,83 @@ extract_outputs() {
   echo "${bullets_array[@]}"
 }
 
+function update_timestamp() {
+  # echo "leo run update \"$TIMESTAMP\" $1\n"
+  cd ../squash_time_oracle
+  run_output=$(leo run update "$TIMESTAMP" $1)
+  outputs=$(extract_outputs "$run_output")
+  TIMESTAMP=${outputs[0]}
+}
+
+function extract_time() {
+  local input="$1"
+  local time=$(echo "$input" | awk -F'time:|u64' '{print $2}' | tr -d '[:space:]')
+  echo "$time"
+}
+
+function water_squash() {
+  # echo -e "leo run water \"$SQUASH\" \"$TIMESTAMP\" $1 10u64\n"
+  cd ../squash_game
+  run_output=$(leo run water "$SQUASH" "$TIMESTAMP" $1 10u64)
+  outputs=$(extract_outputs "$run_output")
+  SQUASH=${outputs[0]}
+}
+
+function extract_kg() {
+  local input="$1"
+  local kg=$(echo "$input" | awk -F'kg:|u64' '{print $2}' | tr -d '[:space:]')
+  # echo "scale=6; $kg / 1000" | bc
+  echo "$kg"
+}
+
 # 1 - mint the first timestamp
-cd squash_time_oracle
 echo "minting initial timestamp..."
-run_output=$(leo run mint 600u64)
+cd ./squash_time_oracle
+run_output=$(leo run mint 0u64)
 outputs=$(extract_outputs "$run_output")
-timestamp=${outputs[0]}
-echo "initial timestamp: $timestamp"
+TIMESTAMP=${outputs[0]}
+echo "\ninitial timestamp: $TIMESTAMP"
+echo "time: $(extract_time $TIMESTAMP)"
 
 # 2 - mint the first squash
-cd ../squash_testing_4
-echo "minting initial squash..."
-echo "leo run mint $PLAYER_ADDRESS \"$timestamp\""
-run_output=$(leo run mint $PLAYER_ADDRESS "$timestamp")
-echo $run_output
+echo "\nminting initial squash..."
+cd ../squash_game
+run_output=$(leo run mint $PLAYER_ADDRESS "$TIMESTAMP")
 outputs=$(extract_outputs "$run_output")
-squash=${outputs[0]}
-echo "initial squash: $squash"
+SQUASH=${outputs[0]}
+echo "\ninitial squash: $SQUASH"
+echo "initial kg: $(extract_kg $SQUASH)"
 
 # 3 - update timestamp
-# 4 - water squash -- kg should be 1
+echo "\nupdating timestamp to 10s..."
+update_timestamp 10u64
+echo "updated timestamp: $TIMESTAMP"
+echo "time: $(extract_time $TIMESTAMP)"
+
+# 4 - water squash -- kg should be 2
+echo "\nwatering squash at 15s..."
+water_squash 15u64
+echo "updated squash: $SQUASH"
+echo "updated kg: $(extract_kg $SQUASH)"
+
 # 5 - update timestamp
-# 6 - update timestamp
+echo "\nupdating timestamp to 20s..."
+update_timestamp 20u64
+echo "updated timestamp: $TIMESTAMP"
+echo "time: $(extract_time $TIMESTAMP)"
+
+# 6 - update timestamp AGAIN
+echo "\nupdating timestamp to 30s..."
+update_timestamp 30u64
+echo "updated timestamp: $TIMESTAMP"
+echo "time: $(extract_time $TIMESTAMP)"
+
 # 7 - water squash -- kg should be 0.5
+echo "\nwatering squash at 35s..."
+water_squash 35u64
+echo "updated squash: $SQUASH"
+echo "updated kg: $(extract_kg $SQUASH)"
+
 # 8 - 
 # 9 - 
 # 19 - 
